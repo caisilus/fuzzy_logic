c1af16b6e73703878b6b972b2ff57abbdeaab90e
AndreyKondakovGW <andreykondakov2302@gmail.com>
2022-05-30 20:06:31 +0300

    add fuzzy AI class

---
+++ Assets/Scripts/FuzzyRule.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Если то
+public class FuzzyRule
+{
+    // [v, a, r, l, f]
+    // [v, a]
+    public FuzzySet[] cause;
+    public FuzzySet consequence;
+    public int[] indexes;
+    public string chain = "AND";
+    //public int inputindex = 0;
+    public int outputindex;
+
+    public FuzzyRule(FuzzySet[] c, FuzzySet cons, int o, int[] i, string ch = "AND")
+    {   
+        cause = c;
+        consequence = cons;
+        indexes = i;
+        chain = ch;
+        outputindex = o;
+    }
+
+    public float fuzzification(float[] input)
+    {
+        float res;
+        float data, prob;
+        if (this.chain == "AND")
+        {
+            res = 1f;
+        }else{
+            res = 0f;
+        }
+
+        for (int i = 0; i < indexes.Length; i++)
+        {
+            data = input[this.indexes[i]];
+            prob = this.cause[i][data];
+
+            if (this.chain == "AND"){
+                res = Mathf.Min(res, prob);
+            }else{
+                res = Mathf.Max(res, prob);
+            }
+
+            //if (res == prob){
+                //this.inputindex = i;
+            //}
+        }
+        return res; 
+    }
+
+    public FuzzySet implement(float[] input){
+        float new_prob = fuzzification(input);
+        return this.consequence.minimize(new_prob);
+    }
+}
---
+++ Assets/Scripts/FuzzySetMain.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class FuzzySet
+{
+    public static FuzzySet max(FuzzySet f1, FuzzySet f2){
+        Dictionary<float, float> data = new Dictionary<float, float>();
+        foreach(KeyValuePair<float, float>  entry in f1.data)
+        {
+            data[entry.Key] = Mathf.Max(f1[entry.Key], f2[entry.Key]);
+        }
+        FuzzySet res = new FuzzySet(data);
+        return res;
+    }
+
+    public static FuzzySet min(FuzzySet f1, FuzzySet f2){
+        Dictionary<float, float> data = new Dictionary<float, float>();
+        foreach(KeyValuePair<float, float>  entry in f1.data)
+        {
+            data[entry.Key] = Mathf.Min(f1[entry.Key], f2[entry.Key]);
+        }
+        FuzzySet res = new FuzzySet(data);
+        return res;
+    }
+
+    public Dictionary<float, float> data =
+    new Dictionary<float, float>();
+    public FuzzySet(Dictionary<float, float> d)
+    {
+        data = d;
+    }
+
+    public float this[float key]
+    {
+        get => GetValue(key);
+        set => SetValue(key, value);
+    }
+
+    private float GetValue(float key){
+        return this.data[key];
+    }
+
+    private void SetValue(float key, float newvalue)
+    {
+        if ((newvalue <= 1) && (newvalue >= 0)){
+            this.data[key] = newvalue;
+        }
+    }
+
+    public FuzzySet minimize(float new_prob)
+    {
+        Dictionary<float, float> data = new Dictionary<float, float>();
+        foreach(KeyValuePair<float, float>  entry in this.data)
+        {
+            data[entry.Key] = Mathf.Min(this.data[entry.Key], new_prob);
+        }
+        FuzzySet res = new FuzzySet(data);
+        return res;
+    }
+
+    public float GetFirstMax(){
+        float maxKey = 0.0f;
+        float maxValue = 0.0f;
+        foreach(float k in this.data.Keys.OrderBy((a)=>a).ToArray())
+        {
+            if (data[k] > maxValue){
+                maxValue = data[k];
+                maxKey = k;
+            }
+        }
+        return maxKey;
+    }
+
+    public override string ToString()
+    {
+        var asString = string.Join("\n", this.data);
+        return asString;
+    }
+}
+
+public class FuzzySetMain : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FuzzySet test = new FuzzySet(new Dictionary<float, float>{{0f, 0.9f}, {1f, 0.5f}});
+        FuzzySet test2 = new FuzzySet(new Dictionary<float, float>{{0f, 0.1f}, {1f, 0.6f}});
+        FuzzySet test3 = new FuzzySet(new Dictionary<float, float>{{0f, 0.1f}, {1f, 0.6f}, {2f, 0.1f}, {3f, 0.6f}});
+
+        Debug.Log("Test1");
+        Debug.Log(test.ToString());
+        Debug.Log(test2.ToString());
+        Debug.Log("Test2");
+        Debug.Log(FuzzySet.max(test, test2));
+        Debug.Log(FuzzySet.min(test, test2));
+        Debug.Log("Test3");
+        Debug.Log(test3.GetFirstMax());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
