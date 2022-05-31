ee6452ba72cb944f38d93a23bf4c3b0ba0ec7aa6
sovsh <soshatokh@yandex.ru>
2022-05-31 21:40:10 +0300

    add disabling sliders when the game started

--- Assets/Scripts/ButtonsScript.cs
+++ Assets/Scripts/ButtonsScript.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonsScript : MonoBehaviour
 {
+    [SerializeField] Slider[] sliders;
+    private void Start()
+    {
+        this.PauseTime();
+    }
     public void PauseTime()
     {
         if (Time.timeScale == 1f)
@@ -12,11 +18,15 @@ public class ButtonsScript : MonoBehaviour
     {
         if (Time.timeScale == 0f)
             Time.timeScale = 1f;
+        foreach (Slider slider in sliders)
+            slider.enabled = false;
     }
     public void RestartTime()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         if (Time.timeScale == 1f)
             Time.timeScale = 0f;
+        foreach (Slider slider in sliders)
+            slider.enabled = true;
     }
 }
